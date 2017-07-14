d7057a57f81d20e5d986961f05faec355b15745d
Ernesto Prieto López <erneplopez@gmail.com>
2017-07-14 04:51:39 +0000

    add password change

--- routes/authentication.js
+++ routes/authentication.js
@@ -3,7 +3,8 @@ var User=require("../models/user"),
     express=require("express"),
     router=express.Router({mergeParams:true}),
     passport=require("passport"),
-    Superuser=require("../models/superuser");
+    Superuser=require("../models/superuser"),
+     Middleware=require("../middleware");
     
 router.get("/login",function(req,res){
      res.render("authentication/login",{page:"login"});
@@ -21,11 +22,35 @@ router.get("/logout",function(req,res){
     req.logout();
     res.redirect("/");
 })
-router.get("/change",function(req,res){
+router.get("/change",Middleware.isLoggedIn,function(req,res){
     req.logout();
     res.render("authentication/change",{page:"change"});
 })
-router.post("/change",function(req,res){
-    //TODO COMPLETE THE CHANGE PASSWORD PIECE
+router.post("/change",passport.authenticate("local",{
+    failureRedirect:"/change"
+    //failureFlash: true
+}),function(req,res){
+    if(req.body.newPassword==req.body.repeatNewPassword){
+        if(req.body.password!=req.body.newPassword){
+            req.user.setPassword(req.body.newPassword,function(err){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    req.user.save();
+                    req.logout();
+                    res.redirect("/login");
+                }
+            })
+        }
+        else{
+           console.log("Same as old one");
+           res.redirect("/change");
+        }
+    }
+    else{
+        console.log("Passwords dont match");
+        res.redirect("/change");
+    }
 })
 module.exports=router;
