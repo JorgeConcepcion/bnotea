527cdcf498aa32a4b14087ad69788f2decf100ab
Ernesto Prieto López <erneplopez@gmail.com>
2017-08-14 17:13:45 -0400

    fix supervision report issue

---
+++ public/scripts/report/supervision.js
@@ -0,0 +1,37 @@
+/* global $ */
+$(".save").on("click",function(e){
+	e.preventDefault();
+	$("#form").get(0).submit(); 
+});
+
+
+
+//Activating validation on all forms
+$.validate({
+	modules: "security",
+	errorMessagePosition : "top",
+	validateOnBlur: false,
+	onSuccess:function(){
+		if($(".assistant-input").attr("value")=="" && $(".assistant-canvas").parent().attr("style")==undefined ||  $(".analyst-input").attr("value")=="" && $(".analyst-canvas").parent().attr("style")==undefined || $(".caregiver-input").attr("value")=="" && $(".caregiver-canvas").parent().attr("style")==undefined ){
+			$.alert("The entire document needs to be signed", {
+				position: ["center", [-0.42, 0]],
+				title: false // title
+			});
+			return false;
+		}
+		else{
+			
+			if($(".analyst").length>0){
+				$("#button").attr("value", "On revision");
+			}
+			if($(".assistant").length>0){
+				$("#button").attr("value", "Completed");
+				
+			}
+			if($(".superuser").length>0){
+				$("#button").attr("value", "Accepted");
+			}
+			return true;
+		}
+	}
+});
